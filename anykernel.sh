87e4c4492673460de41689aeea105829475340d2
Matheus Araújo <apenasmatheus1@gmail.com>
2024-02-26 04:19:55 -0300

    Nightsilver time!

--- anykernel.sh
+++ anykernel.sh
@@ -4,9 +4,9 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=uvite
+kernel.string=Nightsilver
 kernel.compiler=AOSP clang
-kernel.made=CHRISL7
+kernel.made=Ascending
 message.word=
 do.devicecheck=1
 do.modules=0
